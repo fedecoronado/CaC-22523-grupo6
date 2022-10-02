100e88754f24279f073cd7c448f927b155687ee5
EzePucheta91 <112581713+EzePucheta91@users.noreply.github.com>
2022-10-02 02:33:32 -0300

    Actualización-Ezequiel
    Subo lo que hice hasta ahora. Necesito mucha ayuda porque tengo más dudas que certezas.

---
+++ Proyecto/slider.js
@@ -0,0 +1,106 @@
+window.onload = function () {
+    // Variables
+    const IMAGENES = [
+        'imagenes/Marcadores.jpg',
+        'imagenes/Anotador.jpg',
+        'imagenes/Boligrafo.jpg'
+    ];
+    const TIEMPO_INTERVALO_MILESIMAS_SEG = 1000;
+    let posicionActual = 0;
+    let $botonRetroceder = document.querySelector('#retroceder');
+    let $botonAvanzar = document.querySelector('#avanzar');
+    let $imagen = document.querySelector('#imagen');
+    let $botonPlay = document.querySelector('#play');
+    let $botonStop = document.querySelector('#stop');
+    let $dot1 = document.querySelector('#dot1');
+    let $dot2 = document.querySelector('#dot2');
+    let $dot3 = document.querySelector('#dot3');
+    let intervalo;
+
+    // Funciones
+
+    /**
+     * Funcion que cambia la foto en la siguiente posicion
+     */
+    function pasarFoto() {
+        if(posicionActual >= IMAGENES.length - 1) {
+            posicionActual = 0;
+        } else {
+            posicionActual++;
+        }
+        renderizarImagen();
+    }
+
+    /**
+     * Funcion que cambia la foto en la anterior posicion
+     */
+    function retrocederFoto() {
+        if(posicionActual <= 0) {
+            posicionActual = IMAGENES.length - 1;
+        } else {
+            posicionActual--;
+        }
+        renderizarImagen();
+    }
+
+    /**
+     * Funcion que actualiza la imagen de imagen dependiendo de posicionActual
+     */
+    function renderizarImagen () {
+        $imagen.style.backgroundImage = `url(${IMAGENES[posicionActual]})`;
+    }
+
+    /**
+     * Activa el autoplay de la imagen
+     */
+    function playIntervalo() {
+        intervalo = setInterval(pasarFoto, TIEMPO_INTERVALO_MILESIMAS_SEG);
+        // Desactivamos los botones de control
+        $botonAvanzar.setAttribute('disabled', true);
+        $botonRetroceder.setAttribute('disabled', true);
+        $botonPlay.setAttribute('disabled', true);
+        $botonStop.removeAttribute('disabled');
+
+    }
+
+    /**
+     * Para el autoplay de la imagen
+     */
+    function stopIntervalo() {
+        clearInterval(intervalo);
+        // Activamos los botones de control
+        $botonAvanzar.removeAttribute('disabled');
+        $botonRetroceder.removeAttribute('disabled');
+        $botonPlay.removeAttribute('disabled');
+        $botonStop.setAttribute('disabled', true);
+    }
+    function currentSlide1() {
+        posicionActual = 0;
+        stopIntervalo();
+        renderizarImagen ();
+    }
+    function currentSlide2() {
+        posicionActual = 1;
+        stopIntervalo();
+        renderizarImagen ();
+    }
+    function currentSlide3() {
+        posicionActual = 2;
+        stopIntervalo();
+        renderizarImagen ();
+    }
+    // Eventos
+    $botonAvanzar.addEventListener('click', pasarFoto);
+    $botonRetroceder.addEventListener('click', retrocederFoto);
+    $botonPlay.addEventListener('click', playIntervalo);
+    $botonStop.addEventListener('click', stopIntervalo);
+    $dot1.addEventListener('click', currentSlide1);
+    $dot2.addEventListener('click', currentSlide2);
+    $dot3.addEventListener('click', currentSlide3);
+    // Iniciar
+    //renderizarImagen();
+    playIntervalo()
+
+
+} 
+
