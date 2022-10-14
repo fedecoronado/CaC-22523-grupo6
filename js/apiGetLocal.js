6ced95f2d6b33a8baf44bcc1183f1fba0abc07e0
fedecoronado <f24041968@gmail.com>
2022-10-14 20:59:47 -0300

    api de logos de clientes
    se cambia la foma de cargar los logs de clientes por una API que trae un JSON con los datos

---
+++ js/apiGetLocal.js
@@ -0,0 +1,27 @@
+const ul = document.getElementById("clientesLogo");
+const url = '/Imagenes/clientes/clientes.json';
+
+function createNode(element) {
+    return document.createElement(element);
+}
+function append(parent, el){
+    return parent.appendChild(el);
+}
+function traer() {
+    fetch(url)
+        .then(res => res.json())
+        .then(datos => {
+            tabla(datos)
+        })
+}
+
+function tabla(datos){
+    // console.log(datos)
+    for(let valor of datos){ 
+        let img = createNode('img');
+        img.setAttribute("src", `${valor.dir}`);
+        img.setAttribute("alt", `${valor.alt}`)
+        append(ul, img);
+    }
+
+}
